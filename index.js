abac0695a2dc03a3e695224e7e6549feffd75f8c
Ruel Neuman <ruelneuman@gmail.com>
2022-02-14 17:02:04 -0800

    add /api/authors route for aggregate authors data

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require('express-async-errors');
 const { PORT } = require('./util/config');
 const { connectToDatabase } = require('./util/db');
 const blogsRouter = require('./controllers/blogs');
+const authorsRouter = require('./controllers/authors');
 const usersRouter = require('./controllers/users');
 const loginRouter = require('./controllers/login');
 const { errorHandler } = require('./util/middleware');
@@ -12,6 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use('/api/blogs', blogsRouter);
+app.use('/api/authors', authorsRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/login', loginRouter);
 app.use(errorHandler);
